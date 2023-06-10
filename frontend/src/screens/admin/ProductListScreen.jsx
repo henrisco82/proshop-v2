df4f02dade69ff620ac521478af87df9b3a11515
Henry Odohi Unah <henry.unah@gmail.com>
2023-06-10 14:02:01 +0200

    added product review, search and carousel

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,8 +1,10 @@
 import { LinkContainer } from 'react-router-bootstrap';
 import { Row, Col, Table, Button } from 'react-bootstrap';
 import { FaEdit, FaPlus, FaTrash } from 'react-icons/fa';
+import { useParams } from 'react-router-dom';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
+import Paginate from '../../components/Paginate';
 import { 
     useGetProductsQuery, 
     useCreateProductMutation,
@@ -11,7 +13,8 @@ import {
 import { toast } from 'react-toastify';
 
 const ProductListScreen = () => {
-  const { data: products, isLoading, error, refetch } = useGetProductsQuery()
+  const { pageNumber } = useParams();
+  const { data, isLoading, error, refetch } = useGetProductsQuery({ pageNumber})
   const [createProduct, { isLoading: loadingCreate }] = useCreateProductMutation()
   const [deleteProduct, { isLoading: loadingDelete }] =
     useDeleteProductMutation();
@@ -67,7 +70,7 @@ const ProductListScreen = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {products.map((product) => (
+                    {data.products.map((product) => (
                         <tr key={product._id}>
                             <td>{product._id}</td>
                             <td>{product.name}</td>
@@ -92,6 +95,7 @@ const ProductListScreen = () => {
                     ))}
                 </tbody>
             </Table>
+            <Paginate pages={data.pages} page={data.page} isAdmin={true} />
             </>
         )}
     </>
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -4,8 +4,12 @@ import { apiSlice } from "./apiSlice";
 export const productsApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getProducts: builder.query({
-            query: () => ({
+            query: ({ pageNumber, keyword }) => ({
                 url: PRODUCTS_URL,
+                params: {
+                    pageNumber,
+                    keyword,
+                },
             }),
             providesTags: ['Products'],
             keepUnusedDataFor: 5,
@@ -45,6 +49,18 @@ export const productsApiSlice = apiSlice.injectEndpoints({
             }),
             providesTags: ['Product'],
           }),
+        createReview: builder.mutation({
+            query: (data) => ({
+                url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+                method: 'POST',
+                body: data,
+            }),
+            invalidatesTags: ['Product'],
+        }),
+        getTopProducts: builder.query({
+            query: () => `${PRODUCTS_URL}/top`,
+            keepUnusedDataFor: 5,
+        }),
     }),
 });
 
@@ -55,4 +71,6 @@ export const {
     useUpdateProductMutation,
     useUploadProductImageMutation,
     useDeleteProductMutation,
+    useCreateReviewMutation,
+    useGetTopProductsQuery,
 } = productsApiSlice;
